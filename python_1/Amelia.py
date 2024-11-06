0c69250e84ec2655b4ca3ac3474de0fdef9e21e3
anandikesan <mia.star.nan@gmail.com>
2024-11-06 18:07:20 -0500

    Uploading the edited version of my alogrithm idea.

---
+++ python_1/Amelia.py
@@ -0,0 +1,15 @@
+# Snake Case
+first_name = "Amelia" # This is a string
+
+
+lastName = "Nandikesan" # This is also a string
+# Camel Case
+nickName = "Amz" # This is another string
+Age = 6 # This is an int
+bankAccountBalance = 6666666.666 #This is a float
+
+print( first_name)
+print ( bankAccountBalance )
+
+if bankAccountBalance > 1000000:
+    print ( "You are rich. Congrats." )
---
+++ python_1/Apple_sauce.py
@@ -0,0 +1,4 @@
+print ("Welcome to the Apple Sauce Program")
+
+numberOfAppleSauceUser = input ("How many apple sauces do you want:")
+print ("The user said", numberOfAppleSauceUser)
