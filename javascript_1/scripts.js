6793412950a6cdd588647ffd11e3deefbff5de0b
anandikesan <mia.star.nan@gmail.com>
2025-02-10 18:05:18 -0500

    I'm redoing this because I forgot to add my Javascript file to the right folder.

---
+++ javascript_1/scripts.js
@@ -0,0 +1,10 @@
+alert("Welcome to My Academic Planner! Click the close button to continue");
+
+let developmentStartYear=2024
+console.log('developmentStartYear:', developmentStartYear);
+if(developmentStartYear>=2025){
+    console.log('Going strong since 2024')
+}
+else {
+    console.log('Established this year')
+}
