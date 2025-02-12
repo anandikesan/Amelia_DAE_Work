f613271a2466356916da77c896ed1a40595c2ccd
anandikesan <mia.star.nan@gmail.com>
2025-02-12 18:09:21 -0500

    I'm redoing this because it was saying that the update was made in my HTML file but it should've also shown up under JavaScript.

--- javascript_1/scripts.js
+++ javascript_1/scripts.js
@@ -1,10 +1,16 @@
 alert("Welcome to My Academic Planner! Click the close button to continue");
 
 let developmentStartYear=2024
-console.log('developmentStartYear:', developmentStartYear);
+console.log('Development started in', developmentStartYear);
 if(developmentStartYear>=2025){
     console.log('Going strong since 2024')
 }
 else {
     console.log('Established this year')
 }
+
+if (developmentStartYear < 2026 || developmentStartYear > 2024)
+    console.log('Welcome to the great development period. You have yet to see what will come with this website.')
+
+let developerName="Amelia Nandikesan"
+console.log('Developed by' , developerName)
